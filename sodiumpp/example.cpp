65ffa5b9ec59adf9585da2e9c51a907739912aa5
Berkus Decker <berkus@madfire.net>
2016-03-20 19:38:23 +0200

    Use namespace std.

--- sodiumpp/example.cpp
+++ sodiumpp/example.cpp
@@ -25,14 +25,15 @@
 #include <string>
 #include <iostream>
 using namespace sodiumpp;
+using namespace std;
 
 int main(int argc, const char ** argv) {
     box_secret_key sk_client;
     box_secret_key sk_server;
 
-    std::cout << "Client key: " << sk_client << std::endl;
-    std::cout << "Server key: " << sk_server << std::endl;
-    std::cout << std::endl;
+    cout << "Client key: " << sk_client << endl;
+    cout << "Server key: " << sk_server << endl;
+    cout << endl;
 
     // Uses predefined nonce type with 64-bit sequential counter 
     // and constant random bytes for the rest
@@ -41,19 +42,19 @@ int main(int argc, const char ** argv) {
 
     nonce64 used_n;
     encoded_bytes boxed = client_boxer.box("Hello, world!\n", used_n);
-    std::cout << "Nonce (hex): " << used_n.get(encoding::hex).bytes << std::endl;
-    std::cout << "Boxed message (z85): " << boxed.to(encoding::z85).bytes << std::endl;
+    cout << "Nonce (hex): " << used_n.get(encoding::hex).bytes << endl;
+    cout << "Boxed message (z85): " << boxed.to(encoding::z85).bytes << endl;
     // Nonce is passed explicitly here, but will also be increased automatically
     // if unboxing happens in the same order as boxing.
     // In a real application this nonce would be passed along with the boxed message.
-    std::string unboxed = server_unboxer.unbox(boxed, used_n);
-    std::cout << "Unboxed message: " << unboxed;
-    std::cout << std::endl;
+    string unboxed = server_unboxer.unbox(boxed, used_n);
+    cout << "Unboxed message: " << unboxed;
+    cout << endl;
 
     boxed = client_boxer.box("From sodiumpp!\n", used_n);
     unboxed = server_unboxer.unbox(boxed, used_n);
-    std::cout << "Nonce (hex): " << used_n.get(encoding::hex).bytes << std::endl;
-    std::cout << "Boxed message (z85): " << boxed.to(encoding::z85).bytes << std::endl;
-    std::cout << "Unboxed message: " << unboxed;
+    cout << "Nonce (hex): " << used_n.get(encoding::hex).bytes << endl;
+    cout << "Boxed message (z85): " << boxed.to(encoding::z85).bytes << endl;
+    cout << "Unboxed message: " << unboxed;
     return 0;
 }
